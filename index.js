7726f26044f28c4b39a7c4cf136bdf3f3f492852
NikDoe <nikd0e@yandex.ru>
2022-07-02 17:17:09 +0300

    feature(m-fight): fetching movie data from OMDb API

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Hi there!');
+const fetchData = async () => {
+	const response = await axios.get('http://www.omdbapi.com/', {
+		params: {
+			apikey: 'ee463b02',
+			s: 'avengers',
+		},
+	});
+
+	console.log(response.data);
+};
+
+fetchData();
