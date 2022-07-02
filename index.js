5d68849466261908c6d5d32235d4650ad6051abc
NikDoe <nikd0e@yandex.ru>
2022-07-02 17:50:13 +0300

    feature(m-fight): add fetching data on input event w/ delay

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
-const fetchData = async () => {
+const input = document.querySelector('input');
+
+const fetchData = async searchTerm => {
 	const response = await axios.get('http://www.omdbapi.com/', {
 		params: {
 			apikey: 'ee463b02',
-			s: 'avengers',
+			s: searchTerm,
 		},
 	});
 
 	console.log(response.data);
 };
 
-fetchData();
+let timerID;
+function onInput(event) {
+	if (timerID) clearTimeout(timerID);
+	timerID = setTimeout(() => {
+		fetchData(event.target.value);
+	}, 1000);
+}
+
+input.addEventListener('input', onInput);
