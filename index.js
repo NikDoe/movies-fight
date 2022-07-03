9201073d99839a1a803b164a878b12a2f2ca6369
NikDoe <nikd0e@yandex.ru>
2022-07-03 20:05:40 +0300

    fix(m-fight): fix the response w/ no image

--- index.js
+++ index.js
@@ -30,13 +30,15 @@ const fetchData = async searchTerm => {
 async function onInput(event) {
 	const movies = await fetchData(event.target.value);
 
+	resultsWrapper.innerHTML = '';
 	dropdown.classList.add('is-active');
 	for (const movie of movies) {
 		const option = document.createElement('a');
+		const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
 
 		option.classList.add('dropdown-item');
 		option.innerHTML = `
-			<img src="${movie.Poster}" alt="poster" />
+			<img src="${imgSrc}" alt="" />
 			${movie.Title}
 		`;
 		resultsWrapper.appendChild(option);
